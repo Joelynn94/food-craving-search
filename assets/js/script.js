9f92767814d8d4b69b2301d2d5fe0f452d9abaaf
Lorna Lynch <lornalynchit@yahoo.com>
2019-12-19 16:25:20 -0500

    added images to assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 var settings = {
 	"async": true,
 	"crossDomain": true,
-	"url": "https://us-restaurant-menus.p.rapidapi.com/menuitems/search?distance=10&lat=41.4993&page=1&q=tacos&lon=-81.6944",
+	"url": "https://us-restaurant-menus.p.rapidapi.com/menuitems/search?distance=10&lat=41.4993&page=1&q=thai&lon=-81.6944",
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
-		"x-rapidapi-key": ""
+		"x-rapidapi-key": "1e75734330msh4f18a06f7943073p1c5894jsn9dec4fab849c"
 	}
 }
 
