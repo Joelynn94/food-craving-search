38eb2370436e743e5a18dd26d2e723dd37886449
Joe Lynn <josephlynn94@gmail.com>
2019-12-19 21:37:07 -0500

    added a couple images with a description

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ function getSettings(zip){
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
-			"x-rapidapi-key": ""
+			"x-rapidapi-key": "bc537ea7d1msh93abd71cf6a16a9p1cc331jsn99a8e1f89000"
 		}
 	}
 }
